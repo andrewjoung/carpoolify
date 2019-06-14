c5f137d073a675f77f14f2b256229098141ca609
Dalinar Kholin <devinpoore@users.noreply.github.com>
2019-06-14 14:23:33 -0700

    landing page conceptually functional

--- map.js
+++ map.js
@@ -1,14 +1,18 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
+
 var infoWindow;
+var lat;
+var long;
+var directionsService;
+var directionDisplay;
+var pos;
+
+var role;
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 18,
         disableDefaultUI: true,
-        //mapTypeId: "roadmap",
         styles: [
             { featureType: "poi", elementType: "labels", stylers: [{ visibility: "off" }] },
             { featureType: "road", elementType: "geometry.stroke", stylers: [{ color: "#1DB954" }] },
@@ -16,79 +20,57 @@ function initMap() {
             { featureType: "water", elementType: "geometry.fill", stylers: [{ color: "#17D4C4" }] },
             { featureType: "landscape.man_made", elementType: "geometry.fill", stylers: [{ color: "#FFFFFF" }] },
             { featureType: "landscape.man_made", elementType: "geometry.stroke", stylers: [{ color: "#16C7B8" }] }
-
-
-
         ]
     });
 
-    // var input = $("#destinationSearch");
-    // var searchBox = new google.maps.places.SearchBox(input);
+    directionsService = new google.maps.DirectionsService;
+    directionDisplay = new google.maps.DirectionsRenderer({
+        map: map,
+        polylineOptions: { strokeColor: "#191414" }
+    });
 
-    // // var autocomplete = new google.maps.places.Autocomplete(input);
-    // // autocomplete.setFields(
-    // //     ['address_components', 'geometry', 'icon', 'name']);
+    function initializeAutocomplete() {
+        var input = document.getElementById('destinationSearch');
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        google.maps.event.addListener(autocomplete, "place_changed", function () {
+            var place = autocomplete.getPlace();
+            console.log(place);
+            lat = place.geometry.location.lat();
+            long = place.geometry.location.lng();
+            console.log("lat: " + lat, "long: " + long);
+            $("#goButton").attr("data-dismiss", "modal");
+        })
+    }
 
-    // var markers = [];
-    // // Listen for the event fired when the user selects a prediction and retrieve
-    // // more details for that place.
-    // searchBox.addListener('places_changed', function () {
-    //     var places = searchBox.getPlaces();
+    google.maps.event.addDomListener(window, "load", initializeAutocomplete);
 
-    //     if (places.length == 0) {
-    //         return;
-    //     }
-
-    //     // Clear out the old markers.
-    //     markers.forEach(function (marker) {
-    //         marker.setMap(null);
-    //     });
-    //     markers = [];
-
-    //     // For each place, get the icon, name and location.
-    //     var bounds = new google.maps.LatLngBounds();
-    //     places.forEach(function (place) {
-    //         if (!place.geometry) {
-    //             console.log("Returned place contains no geometry");
-    //             return;
-    //         }
-    //         var icon = {
-    //             url: place.icon,
-    //             size: new google.maps.Size(71, 71),
-    //             origin: new google.maps.Point(0, 0),
-    //             anchor: new google.maps.Point(17, 34),
-    //             scaledSize: new google.maps.Size(25, 25)
-    //         };
-
-    //         // Create a marker for each place.
-    //         markers.push(new google.maps.Marker({
-    //             map: map,
-    //             icon: icon,
-    //             title: place.name,
-    //             position: place.geometry.location
-    //         }));
-
-    //         if (place.geometry.viewport) {
-    //             // Only geocodes have viewport.
-    //             bounds.union(place.geometry.viewport);
-    //         } else {
-    //             bounds.extend(place.geometry.location);
-    //         }
-    //     });
-    //     map.fitBounds(bounds);
-    // });
-
+    $("#goButton").on("click", function() {
+        $("#mapButtons").css("display", "none");
+        
+        var request = {
+            origin: pos.lat + "," + pos.lng,
+            destination: lat + "," + long,
+            travelMode: 'DRIVING'
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == 'OK') {
+                directionDisplay.setDirections(response);
+            }
+        });
+    });
 
 
     infoWindow = new google.maps.InfoWindow;
-
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
+            pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
+            console.log(navigator.geolocation);
+            console.log(position);
+            console.log("current lat: " + pos.lat, "current long:" + pos.lng);
 
             //infoWindow.setPosition(pos);
             //infoWindow.setContent('Location found.');
@@ -103,6 +85,7 @@ function initMap() {
     }
 }
 
+//
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
@@ -110,3 +93,4 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
 }
+
